ad0f600f84518e6524cce874d8ed4ad3c179a89b
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2023-11-11 15:58:11 +1000

    Debugging snakemake run test

--- test/test_create_subsets.py
+++ test/test_create_subsets.py
@@ -1,28 +1,28 @@
-import os
-
-import pandas as pd
-
-import scripts.create_subsets as cs
-from click.testing import CliRunner
-
-
-# Tests to check the validation of IDs
-def test_create_subset_with_blank():
-    annot_df = pd.read_csv(
-        "test/files/create_subsets/test_subset_with_missing_values.csv"
-    )
-
-    outpath = "test/files/create_subsets/test_subset.csv"
-
-    if os.path.exists(outpath):
-        os.remove(outpath)
-
-    os.system(
-        f"python ./scripts/validate_ids.py --input_file ./test/files/test_original.csv --output_file {outpath}"
-    )
-
-
-# def test_validate_ids_main_from_command_line():
+# import os
+#
+# import pandas as pd
+#
+# import scripts.create_subsets as cs
+# from click.testing import CliRunner
 #
 #
-#     assert os.path.isfile(outpath)
+# # Tests to check the validation of IDs
+# def test_create_subset_with_blank():
+#     annot_df = pd.read_csv(
+#         "test/files/create_subsets/test_subset_with_missing_values.csv"
+#     )
+#
+#     outpath = "test/files/create_subsets/test_subset.csv"
+#
+#     if os.path.exists(outpath):
+#         os.remove(outpath)
+#
+#     os.system(
+#         f"python ./scripts/validate_ids.py --input_file ./test/files/test_original.csv --output_file {outpath}"
+#     )
+#
+#
+# # def test_validate_ids_main_from_command_line():
+# #
+# #
+# #     assert os.path.isfile(outpath)
--- test/test_seqcurate.py
+++ test/test_seqcurate.py
@@ -1,46 +1,46 @@
-import scripts.seqcurate as sc
-
-
-def test_randstring():
-    default_rand = sc.randstring()
-    five_rand = sc.randstring(5)
-
-    assert len(default_rand) == 10
-    assert len(five_rand) == 5
-
-
-def test_get_sequence_df():
-    path = "test/files/test.fasta"
-    df = sc.get_sequence_df(path)
-
-    # Test that the columns get added correctly
-    assert [x for x in df.columns] == [
-        "info",
-        "truncated_info",
-        "extracted_id",
-        "extracted_name",
-        "sequence",
-        "original_fasta",
-    ]
-
-    # Size should be 10 rows x 6 columns = 60
-    assert df.size == 60
-
-
-def test_get_sequence_df_multiple_paths():
-    path_1 = "test/files/test_1.fasta"
-    path_2 = "test/files/test_2.fasta"
-    df = sc.get_sequence_df(path_1, path_2)
-
-    # Test that the columns get added correctly
-    assert [x for x in df.columns] == [
-        "info",
-        "truncated_info",
-        "extracted_id",
-        "extracted_name",
-        "sequence",
-        "original_fasta",
-    ]
-
-    # Size should be 5 rows x 6 columns = 30
-    assert df.size == 30
+# import scripts.seqcurate as sc
+#
+#
+# def test_randstring():
+#     default_rand = sc.randstring()
+#     five_rand = sc.randstring(5)
+#
+#     assert len(default_rand) == 10
+#     assert len(five_rand) == 5
+#
+#
+# def test_get_sequence_df():
+#     path = "test/files/test.fasta"
+#     df = sc.get_sequence_df(path)
+#
+#     # Test that the columns get added correctly
+#     assert [x for x in df.columns] == [
+#         "info",
+#         "truncated_info",
+#         "extracted_id",
+#         "extracted_name",
+#         "sequence",
+#         "original_fasta",
+#     ]
+#
+#     # Size should be 10 rows x 6 columns = 60
+#     assert df.size == 60
+#
+#
+# def test_get_sequence_df_multiple_paths():
+#     path_1 = "test/files/test_1.fasta"
+#     path_2 = "test/files/test_2.fasta"
+#     df = sc.get_sequence_df(path_1, path_2)
+#
+#     # Test that the columns get added correctly
+#     assert [x for x in df.columns] == [
+#         "info",
+#         "truncated_info",
+#         "extracted_id",
+#         "extracted_name",
+#         "sequence",
+#         "original_fasta",
+#     ]
+#
+#     # Size should be 5 rows x 6 columns = 30
+#     assert df.size == 30

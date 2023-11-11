1744fff5a1129107871662d46b00ca80f565a15a
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2023-11-11 16:15:40 +1000

    add more to the files

--- test/test_snakemake.py
+++ test/test_snakemake.py
@@ -103,6 +103,18 @@ def test_snakemake_pipeline_with_existing_annotation_file(tmpdir):
     scripts_dest = os.path.join(test_dir, "scripts")
     subset_output = os.path.join(test_dir, "subsets")
 
+
+        # Remove snakefile
+        if os.path.exists(snakefile_dest):
+            os.remove(snakefile_dest)
+
+        if os.path.exists(scripts_dest):
+            shutil.rmtree(scripts_dest)
+
+        if os.path.exists(subset_output):
+            shutil.rmtree(subset_output)
+
+
     # Copy Snakefile from your Git repository to the test directory
     shutil.copyfile("snakefile", snakefile_dest)
 
@@ -116,8 +128,15 @@ def test_snakemake_pipeline_with_existing_annotation_file(tmpdir):
 
     print ('okay')
 
+    shutil.copyfile("snakefile", snakefile_dest)
+    shutil.copytree("scripts", scripts_dest)
+
     os.system("snakemake --cores 1 --configfile test/files/config/test_config_keep_annotations.yaml")
 
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/all_kari/1/grasp_results/GRASP_ancestors.fa"))
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/1/grasp_results/GRASP_ancestors.fa"))
 
 
     assert os.path.exists(os.path.join(subset_output, "all_kari/1/grasp_results/GRASP_ancestors.fa"))

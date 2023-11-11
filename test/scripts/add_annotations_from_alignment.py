c06758ad183321dc896390c21dba70fe37da23c9
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2023-11-11 15:47:57 +1000

    Reconfigure full snakemake run tests to pass

--- test/scripts/add_annotations_from_alignment.py
+++ test/scripts/add_annotations_from_alignment.py
@@ -1,25 +1,27 @@
-import annot_functions as an
 import pandas as pd
 from Bio import AlignIO
 import seqcurate as sc
 
-df = pd.read_csv(snakemake.input.csv)
 
-aln = AlignIO.read(snakemake.input.aln, format="fasta")
-aln_dict = {seq.name: str(seq.seq) for seq in aln}
+def main():
+    df = pd.read_csv(snakemake.input.csv)
 
-align_df = sc.get_sequence_df(snakemake.input.aln, alignment=True, ancestor=True)
+    # Get a sequence DataFrame using seqcurate
+    align_df = sc.get_sequence_df(snakemake.input.aln, alignment=True, ancestor=True)
+
+    # Merge the data from the CSV file and the sequence DataFrame based on the "info" column
+    merged_df = pd.merge(
+        df,
+        align_df,
+        left_on=["info"],
+        right_on=["info"],
+        suffixes=["", "_r"],
+    )
+
+    # Write the merged DataFrame to a CSV file
+    merged_df.to_csv(snakemake.output.csv, index=False)
 
 
-# IF CUSTOMISING THIS FILE PLACE CUSTOM CODE HERE
-
-
-merged_df = pd.merge(
-    df,
-    align_df,
-    left_on=["info"],
-    right_on=["info"],
-    suffixes=["", "_r"],
-)
-
-merged_df.to_csv(snakemake.output.csv, index=False)
+# Check if this script is being run as the main program
+if __name__ == "__main__":
+    main()
--- test/test_snakemake.py
+++ test/test_snakemake.py
@@ -1,79 +1,79 @@
-# import os
-# import shutil
-#
-#
-# def test_snakemake_pipeline():
-#
-#     # Move the snakefile into the test directory
-#     snakefile_dest = "test/snakefile"
-#
-#     scripts_dest = "test/scripts"
-#     snakemake_output = "test/files/test_workflow/example_workflow"
-#
-#     # Remove snakefile
-#     if os.path.exists(snakefile_dest):
-#         os.remove(snakefile_dest)
-#
-#     if os.path.exists(scripts_dest):
-#         shutil.rmtree(scripts_dest)
-#
-#     if os.path.exists(snakemake_output):
-#         shutil.rmtree(snakemake_output)
-#
-#     print ("Copy snakefile and scripts2 folder into test directory")
-#
-#     shutil.copyfile("snakefile", snakefile_dest)
-#     shutil.copytree("scripts", scripts_dest)
-#
-#     os.system("snakemake --cores 1 --configfile test/files/config/test_config.yaml")
-#
-#     assert(os.path.exists("test/files/test_workflow/example_workflow/datasets/test_ec_1_1_1_86/subsets/all_kari/grasp_results/GRASP_ancestors.fa"))
-#     assert(os.path.exists("test/files/test_workflow/example_workflow/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/grasp_results/GRASP_ancestors.fa"))
-#
-#
-#     # snakemake_output = "./files/test_workflow/example_workflow"
-#     # Remove snakefile
-#     # if os.path.exists(snakefile_dest):
-#     #     os.remove(snakefile_dest)
-#     #
-#     # shutil.rmtree(scripts_dest)
-#     #
-#     # shutil.rmtree(snakemake_output)
-#
-# def test_snakemake_pipeline_with_existing_annotation_file():
-#     # This tests the full snakemake pipeline after going online to the databases to build the annotation files - so it should be quicker to run
-#     # Move the snakefile into the test directory
-#     snakefile_dest = "test/snakefile"
-#
-#     scripts_dest = "test/scripts"
-#
-#     subset_output = "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets"
-#     summary_output = "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/dataset_summary"
-#
-#     # Remove snakefile
-#     if os.path.exists(snakefile_dest):
-#         os.remove(snakefile_dest)
-#
-#     if os.path.exists(scripts_dest):
-#         shutil.rmtree(scripts_dest)
-#
-#     if os.path.exists(subset_output):
-#         shutil.rmtree(subset_output)
-#
-#     if os.path.exists(summary_output):
-#         shutil.rmtree(summary_output)
-#
-#
-#
-#     print("Copy snakefile and scripts2 folder into test directory")
-#
-#     shutil.copyfile("snakefile", snakefile_dest)
-#     shutil.copytree("scripts", scripts_dest)
-#
-#     os.system("snakemake --cores 1 --configfile test/files/config/test_config_keep_annotations.yaml")
-#
-#     assert (os.path.exists(
-#         "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/all_kari/grasp_results/GRASP_ancestors.fa"))
-#     assert (os.path.exists(
-#         "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/grasp_results/GRASP_ancestors.fa"))
-#
+import os
+import shutil
+
+
+def test_snakemake_pipeline():
+
+    # Move the snakefile into the test directory
+    snakefile_dest = "test/snakefile"
+
+    scripts_dest = "test/scripts"
+    snakemake_output = "test/files/test_workflow/example_workflow"
+
+    # Remove snakefile
+    if os.path.exists(snakefile_dest):
+        os.remove(snakefile_dest)
+
+    if os.path.exists(scripts_dest):
+        shutil.rmtree(scripts_dest)
+
+    if os.path.exists(snakemake_output):
+        shutil.rmtree(snakemake_output)
+
+    print ("Copy snakefile and scripts folder into test directory")
+
+    shutil.copyfile("snakefile", snakefile_dest)
+    shutil.copytree("scripts", scripts_dest)
+
+    os.system("snakemake --cores 1 --configfile test/files/config/test_config.yaml")
+
+    assert(os.path.exists("test/files/test_workflow/example_workflow/datasets/test_ec_1_1_1_86/subsets/all_kari/1/grasp_results/GRASP_ancestors.fa"))
+    assert(os.path.exists("test/files/test_workflow/example_workflow/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/1/grasp_results/GRASP_ancestors.fa"))
+
+
+    # snakemake_output = "./files/test_workflow/example_workflow"
+    # Remove snakefile
+    # if os.path.exists(snakefile_dest):
+    #     os.remove(snakefile_dest)
+    #
+    # shutil.rmtree(scripts_dest)
+    #
+    # shutil.rmtree(snakemake_output)
+
+def test_snakemake_pipeline_with_existing_annotation_file():
+    # This tests the full snakemake pipeline without going online to the databases to build the annotation files - so it should be quicker to run
+    # Move the snakefile into the test directory
+    snakefile_dest = "test/snakefile"
+
+    scripts_dest = "test/scripts"
+
+    subset_output = "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets"
+    summary_output = "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/dataset_summary"
+
+    # Remove snakefile
+    if os.path.exists(snakefile_dest):
+        os.remove(snakefile_dest)
+
+    if os.path.exists(scripts_dest):
+        shutil.rmtree(scripts_dest)
+
+    if os.path.exists(subset_output):
+        shutil.rmtree(subset_output)
+
+    # if os.path.exists(summary_output):
+    #     shutil.rmtree(summary_output)
+
+
+
+    print("Copy snakefile and scripts folder into test directory")
+
+    shutil.copyfile("snakefile", snakefile_dest)
+    shutil.copytree("scripts", scripts_dest)
+
+    os.system("snakemake --cores 1 --configfile test/files/config/test_config_keep_annotations.yaml")
+
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/all_kari/1/grasp_results/GRASP_ancestors.fa"))
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/1/grasp_results/GRASP_ancestors.fa"))
+

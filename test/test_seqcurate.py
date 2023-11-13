65407a5f73c0b58799cdf2260c046013c3bd6ce1
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2023-11-13 11:27:37 +1000

    Add get_seq_content test

--- test/test_seqcurate.py
+++ test/test_seqcurate.py
@@ -1,5 +1,29 @@
 import scripts.seqcurate as sc
+import pytest
+import os
+import tempfile
+@pytest.fixture
+def temporary_fasta_file():
+    # Create a temporary FASTA file with test data
+    fasta_content = ">seq1\nPPGN\n>seq2\nPPGD"
+    fasta_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    fasta_file.write(fasta_content)
+    fasta_file.close()
+    yield fasta_file.name
 
+    # Remove the temporary FASTA file
+    os.remove(fasta_file.name)
+
+def test_get_sequence_content_from_fasta(temporary_fasta_file):
+    # Call the function with the temporary FASTA file
+    sequences = sc.get_sequence_content_from_fasta(temporary_fasta_file)
+
+    # Check if the returned sequences match the expected ones
+    expected_sequences = ["PPGN", "PPGD"]
+    assert sequences == expected_sequences
+
+if __name__ == '__main__':
+    pytest.main()
 
 def test_randstring():
     default_rand = sc.randstring()

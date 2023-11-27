d5b3f53ae3c8af2f4910f9a5f414a1ba99141bad
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2023-11-27 10:44:48 +1000

    Debugging tests

--- test/test_snakemake.py
+++ test/test_snakemake.py
@@ -119,7 +119,50 @@ def test_snakemake_pipeline_with_existing_annotation_file(tmpdir):
 
     print (os.path.exists(snakefile_dest))
 
+    print ('okay')    # Use tmpdir provided by pytest to create temporary directories
+    test_dir = tmpdir.mkdir("test_workflow")
+    snakefile_dest = os.path.join(test_dir, "snakefile")
+    scripts_dest = os.path.join(test_dir, "scripts")
+    subset_output = os.path.join(test_dir, "subsets")
+
+    # Remove snakefile
+    if os.path.exists(snakefile_dest):
+        os.remove(snakefile_dest)
+
+    if os.path.exists(scripts_dest):
+        shutil.rmtree(scripts_dest)
+
+    if os.path.exists(subset_output):
+        shutil.rmtree(subset_output)
+
+
+    print ('snakefile dest is')
+    print (snakefile_dest)
+
+    print (os.path.exists(snakefile_dest))
+
     print ('okay')
+
+    shutil.copyfile("snakefile", snakefile_dest)
+    shutil.copytree("scripts", scripts_dest)
+
+    print (os.path.exists(snakefile_dest))
+    print (os.path.exists(scripts_dest))
+
+    print ('current working dir')
+    print (os.getcwd())
+
+
+    os.system("snakemake --cores 1 --configfile test/files/config/test_config_keep_annotations.yaml")
+
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/all_kari/1/grasp_results/GRASP_ancestors.fa"))
+    assert (os.path.exists(
+        "test/files/test_workflow/keep_annotations/datasets/test_ec_1_1_1_86/subsets/eukaryotic_test/1/grasp_results/GRASP_ancestors.fa"))
+
+
+    assert os.path.exists(os.path.join(subset_output, "all_kari/1/grasp_results/GRASP_ancestors.fa"))
+    assert os.path.exists(os.path.join(subset_output, "eukaryotic_test/1/grasp_results/GRASP_ancestors.fa"))
 
     shutil.copyfile("snakefile", snakefile_dest)
     shutil.copytree("scripts", scripts_dest)
